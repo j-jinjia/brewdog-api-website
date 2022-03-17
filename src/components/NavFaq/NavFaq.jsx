2dbf501f83496f092ea7cfc7d5b5668b481bcf69
j-jinjia <j.jinjia99@gmail.com>
2022-03-17 18:04:30 +0100

    last touches on responsivness and stylings

--- src/components/NavFaq/NavFaq.jsx
+++ src/components/NavFaq/NavFaq.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./NavFaq.scss";
-import cross from "../../assets/images/white-cross.png";
+import cross from "../../assets/images/black-cross.png";
+import warning from "../../assets/images/drink-responsably.png";
 const NavFaq = ({ handleFaqClick }) => {
   return (
     <div className="help">
@@ -10,8 +11,21 @@ const NavFaq = ({ handleFaqClick }) => {
         alt="white-cross"
         onClick={handleFaqClick}
       />
-      <h3 className="help__title">Need help?</h3>
-      <p className="help__subtitle">Let me show you around!</p>
+      <div className="content">
+        <h3 className="content__title">Need help?</h3>
+        <p className="content__subtitle">Let me show you around!</p>
+        <br />
+        <p className="content__content">
+          {" "}
+          In this website you can search for your favourite BrewDog beer. <br />
+          You will be able to see basic information about each beer and some
+          extra information once you click on View More. <br />
+          <br />
+          Can't find your perfect drink? Try using the search box <br />
+          Want to search with a specific criteria? Try using our filters.
+        </p>
+        <img src={warning} alt="drink-responsably" className="content-image" />
+      </div>
     </div>
   );
 };
