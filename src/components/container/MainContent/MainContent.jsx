d3a002efffede8330dbdab2398b5261e1c4d1a3a
Jiazheng Jin Jia <j.jinjia99@gmail.com>
2022-04-30 00:03:25 +0200

    first try

--- src/components/container/MainContent/MainContent.jsx
+++ src/components/container/MainContent/MainContent.jsx
@@ -6,6 +6,9 @@ import Filters from "../../Filters/Filters";
 
 const MainContent = () => {
   const [beers, setBeers] = useState([]);
+  const [beersFilteredABV, setBeersFilteredABV] = useState([]);
+  const [beersFilteredClassic, setBeersFilteredClassic] = useState([]);
+  const [beersFilteredPh, setBeersFilteredPh] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
   const url = "https://api.punkapi.com/v2/beers?page=2&per_page=80";
@@ -26,24 +29,29 @@ const MainContent = () => {
   };
 
   const filteredBeers = beers.filter((beer) => {
+    if (searchTerm) {
+    }
     const beerNameLower = beer.name.toLowerCase();
 
     return beerNameLower.includes(searchTerm);
   });
+
   const handleCheckedABV = (event) => {
     event.target.checked
-      ? setBeers(beers.filter((beer) => beer.abv > 6))
-      : getBeers(beers);
+      ? setBeersFilteredABV(beers.filter((beer) => beer.abv > 6))
+      : setBeersFilteredABV(beers);
   };
   const handleCheckedClassic = (event) => {
     event.target.checked
-      ? setBeers(beers.filter((beer) => beer.first_brewed.split("/")[1] < 2010))
-      : getBeers(beers);
+      ? setBeersFilteredClassic(
+          beers.filter((beer) => beer.first_brewed.split("/")[1] < 2010)
+        )
+      : setBeersFilteredClassic(beers);
   };
   const handleCheckedPH = (event) => {
     event.target.checked
-      ? setBeers(beers.filter((beer) => beer.ph < 4))
-      : getBeers(beers);
+      ? setBeersFilteredPh(beers.filter((beer) => beer.ph < 4))
+      : setBeersFilteredPh(beers);
   };
 
   return (
@@ -57,6 +65,7 @@ const MainContent = () => {
         <Filters
           handleCheckABV={handleCheckedABV}
           handleCheckClassic={handleCheckedClassic}
+          S
           handleCheckPH={handleCheckedPH}
         />
       </div>
